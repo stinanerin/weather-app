c000cba205b14135e66245fa94c319ce087cd6fa
stormstina <stina.nerin@gmail.com>
2023-08-09 20:29:35 +0200

    Add Link around logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,24 @@
-import logo from '../assets/logo-mini.svg'; 
+import { Link } from "react-router-dom";
+import logo from "../assets/logo-mini.svg";
 import Search from "./Search";
 
 interface SearchProps {
-  onSearchResultClick: (
-      location: string,
-      latitude: number,
-      longitude: number
-  ) => void;
+    onSearchResultClick: (
+        location: string,
+        latitude: number,
+        longitude: number
+    ) => void;
 }
 
-const Header = ({onSearchResultClick} : SearchProps) => {
-  return (
-    <header className="header">
+const Header = ({ onSearchResultClick }: SearchProps) => {
+    return (
+        <header className="header">
+            <Link to="/">
+                <img className="logo" src={logo} alt="Logo" />
+            </Link>
+            <Search onSearchResultClick={onSearchResultClick} />
+        </header>
+    );
+};
 
-        <img className="logo" src={logo} alt="Logo" />
-        <Search onSearchResultClick={onSearchResultClick} />
-        
-    </header>
-  )
-}
-
-export default Header
+export default Header;
