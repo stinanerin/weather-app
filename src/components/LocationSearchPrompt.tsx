a4011eb8558ad55cfeb1e6be9bc351a4fa6966f5
stormstina <stina.nerin@gmail.com>
2023-08-14 22:47:37 +0200

    Add alert component

--- src/components/LocationSearchPrompt.tsx
+++ src/components/LocationSearchPrompt.tsx
@@ -1,17 +1,17 @@
+import Alert from "./Alert/Alert";
+
 const LocationSearchPrompt = () => {
+    const msg = `We understand that sometimes granting location permission can be
+    a concern. No worries! You can still get
+    weather information by searching for a specific location.
+    `;
+
     return (
-        <div className="text-center">
-
-            <h1 className="weather-heading"> 🌦️ Welcome to our Weather Application! 🌈</h1>
-
-            <h2>❌ Location Access Not Granted ❌</h2>
-            <p>
-                We understand that sometimes granting location permission can be
-                a concern. No worries! You're in control. You can still get
-                weather information by searching for a specific location.
-            </p>
-            <br/>
-            <p>Happy weather exploring! 🌧️☀️❄️</p>
+        <div className="elem-center">
+            <h1 className="weather-heading">
+                🌦️ Welcome to our Weather Application! 🌈
+            </h1>
+            <Alert message={msg} heading="ℹ️  Location Access Not Granted" />
         </div>
     );
 };
