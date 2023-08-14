243f10147990b208353c8d17b4e69b1ce2471258
stormstina <stina.nerin@gmail.com>
2023-08-14 17:50:30 +0200

    Test: switch BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom';
 
 import { getLocation } from "./utility/helper";
 import { fetchData } from "./utility/api";
